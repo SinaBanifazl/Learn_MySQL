eac58a1e8b07389001f30b4883029e05969eb4c7
sinabanifazl <fjsalfjlsafjas@hafkaf>
2025-07-21 10:59:53 +0330

    fix previous commit

--- Train.sql
+++ Train.sql
@@ -2,7 +2,7 @@ SELECT c.customer_id, c.first_name, o.order_id
 FROM customers c
 LEFT JOIN orders o
 	ON c.customer_id = o.customer_id
-JOIN shippers sh
+LEFT JOIN shippers sh
 	ON o.shipper_id = sh.shipper_id
     
 -- we have the previous problem
